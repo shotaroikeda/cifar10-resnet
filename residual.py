d4f4056240d9c84a0724c070dd245c6aee02191a
shotaroikeda <ikeda.shot@gmail.com>
2017-04-03 09:01:10 -0500

    fixed final layer and training params

--- residual.py
+++ residual.py
@@ -33,12 +33,12 @@ def _res_block(input, dims, name, training):
             res_1 = _conv2d(input, _init_conv(dims, "W1"))
 
         batch_1 = tf.nn.relu(tf.layers.batch_normalization(res_1,
-                                                           name = "batch_norm_1", trainable = training))
+                                                           name = "batch_norm_1", training = training))
         dims[2] = dims[3] # Change the dimension after the first conv
         W2 = _init_conv(dims, "W2")
         # BN before non-linearlity
         res_out = tf.layers.batch_normalization(_conv2d(batch_1, W2),
-                                                name = "batch_norm_2", trainable = training)
+                                                name = "batch_norm_2", training = training)
 
     if diff:
         shrink_pool = tf.nn.avg_pool(input, ksize=[1,2,2,1], strides=[1,2,2,1], padding = 'SAME')
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     init_out = tf.nn.relu(tf.layers.batch_normalization(
         _conv2d(x, W_init),
         name = "batch_norm_0",
-        trainable = training
+        training = training
     ))
 
     # Residual block 1
@@ -120,20 +120,12 @@ if __name__ == '__main__':
     avg1 = tf.nn.avg_pool(res3, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
     avg1_reshape = tf.reshape(avg1, [-1, 4*4*64])
 
-    W_fc = _init_weight([4*4*64, 1000], "W_fc1")
-    b_fc = _init_weight([1000], "b_fc1")
-    fc1 = tf.nn.relu(tf.layers.batch_normalization(tf.matmul(avg1_reshape, W_fc) + b_fc,
-                                                   name = "fc1_batch_norm",
-                                                   trainable = training))
-
-    W_out = _init_weight([1000, 10], "W_out")
-    b_out = _init_weight([10], "b_out")
-    out = tf.nn.relu(tf.layers.batch_normalization(tf.matmul(fc1, W_out) + b_out,
-                                                   name = "fc_out_batch_norm",
-                                                   trainable = training))
+    W_fc = _init_weight([4*4*64, 10], "W_fc1")
+    b_fc = _init_weight([10], "b_fc1")
+    fc1 = tf.matmul(avg1_reshape, W_fc) + b_fc
 
     cross_entropy = tf.reduce_mean(
-        tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_, logits=out)
+        tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_, logits=fc1)
     )
 
     training_rate = tf.placeholder(tf.float32, shape=[])
